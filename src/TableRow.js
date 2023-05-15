63c815d845bd5ce82539439a087c79cd468dc94d
David Asulin <dudyas6@gmail.com>
2023-05-15 14:48:25 +0300

    changes
    small changes

--- src/TableRow.js
+++ src/TableRow.js
@@ -10,13 +10,13 @@ export default function TableRow({ index, item }) {
       <td className="border px-4 py-2">{item.currentQuantity}</td>
       <td className="border px-4 py-2">{item.minimumQuantity}</td>
       <td className="border px-4 py-2      items-center justify-center">
-        <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
+        <a href='# ' className="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
           <i class="fas fa-eye"></i>
         </a>
-        <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
+        <a href='# ' class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
           <i class="fas fa-edit"></i>
         </a>
-        <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-red-500">
+        <a href='# ' class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-red-500">
           <i class="fas fa-trash"></i>
         </a>
       </td>
