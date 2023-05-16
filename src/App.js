38ef2508d5965cc608ca15177d53bbd8ee8c4210
David Asulin <dudyas6@gmail.com>
2023-05-16 12:43:23 +0300

    mongozibi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
-
+import axios from "axios";
 import Header from "./Header";
 import MainBody from "./MainBody";
 import Sidebar from "./Sidebar";
+import { useEffect } from "react";
 function App() {
+  
+  useEffect(() => {
+    const connectToMongoDB = async () => {
+      try {
+        const response = await axios.get("/api/connect");
+        console.log(response.data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+    connectToMongoDB();
+  }, []);
+
   return (
     <div className="">
       <div className="mx-auto bg-grey-400">
