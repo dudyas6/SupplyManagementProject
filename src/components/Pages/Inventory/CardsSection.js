f0a1b987d2f364f7f34152efa9e1be811c1e7013
Lidor <100760730+PoiZeX@users.noreply.github.com>
2023-06-04 23:43:19 +0300

    filter works on cards section

--- src/components/Pages/Inventory/CardsSection.js
+++ src/components/Pages/Inventory/CardsSection.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCard from "./ItemCard";
-import FilterComponent from "../../../common/FilterComponent";
+import FilterForm from "../../../common/FilterForm";
 import { Card } from "../../../common/Elements";
 import AddItemPopup from "./AddItemPopup";
-export default function CardsSection({ items, orders, handleChangeItems }) {
+export default function CardsSection({ items, handleChangeItems }) {
   const [showPopup, setShowPopup] = React.useState(false);
+  const [filteredOrders, setFilteredOrders] = useState([]);
 
   function GenerateCards() {
-    return items.map((item) => (
+    const itemsToIterate = filteredOrders ? filteredOrders : items;
+    return itemsToIterate.map((item) => (
       <ItemCard
         key={item.ItemId}
         item={item}
@@ -16,22 +18,83 @@ export default function CardsSection({ items, orders, handleChangeItems }) {
     ));
   }
 
-  function onFilter(filters) {
-    console.log("Im in onFilter!!!");
+  const handleFilter = (filters) => {
+    // when filter component change something
+
+    if (filters === null) {
+      setFilteredOrders(items); // clear filters
+      return;
+    }
+
+    const filterItemName = filters["ItemName"];
+    const filterQuantityRange = filters["CurrentQuantity"];
+    const filterPrice = filters["Price"];
+
+    const filteredOrders = items.filter((item) => {
+      // The actual filter
+
+      return (
+        (filterItemName ? item.ItemName.includes(filterItemName) : true) &&
+        (filterQuantityRange
+          ? item.CurrentQuantity >= filterQuantityRange["start"] &&
+            item.CurrentQuantity <= filterQuantityRange["end"]
+          : true) &&
+        (filterPrice
+          ? item.Price >= filterPrice["start"] &&
+            item.Price <= filterPrice["end"]
+          : true)
+      );
+    });
+
+    setFilteredOrders(filteredOrders);
+  };
+
+  const getMaxCurrentQuantity = () => {
+    if (items === undefined) return;
+    const maxCurrentQuantity = items.reduce(
+      (max, item) => (item.CurrentQuantity > max ? item.CurrentQuantity : max),
+      0
+    );
+    return maxCurrentQuantity;
+  };
+  const getMaxPrice = () => {
+    if (items === undefined) return;
+    const maxPrice = items.reduce(
+      (max, item) => (item.Price > max ? item.Price : max),
+      0
+    );
+    return maxPrice;
+  };
+  function getAllItemsNames() {
+    const uniqueNames = new Set();
+    items.forEach((item) => {
+      uniqueNames.add(item.ItemName);
+    });
+    return Array.from(uniqueNames);
   }
 
+  const filterConfig = {
+    ItemName: { type: "Dropdown", options: getAllItemsNames() },
+    CurrentQuantity: { type: "Range", min: 0, max: getMaxCurrentQuantity() },
+    Price: { type: "Range", min: 0, max: getMaxPrice() },
+  };
   return (
     <>
-        <div className="w-full mt-4 mb-2 inline-block">
-          <button
-            onClick={() => setShowPopup(true)}
-            className="float-right w-52 h-24 px-4 py-2 font-bold text-white bg-green-500 rounded-3xl hover:bg-green-700"
-          >
-            + Create Item
-          </button>
-        </div>
+      <div className="w-full mt-4 mb-2 inline-block">
+        <button
+          onClick={() => setShowPopup(true)}
+          className="float-right w-52 h-24 px-4 py-2 font-bold text-white bg-green-500 rounded-3xl hover:bg-green-700"
+        >
+          + Create Item
+        </button>
+      </div>
       <Card>
-        <FilterComponent data={items} onFilter={onFilter} />
+        {/* <FilterComponent data={items} onFilter={onFilter} /> */}
+        <FilterForm
+          data={items}
+          onFilter={handleFilter}
+          filterConfig={filterConfig}
+        />
       </Card>
       {showPopup && <AddItemPopup />}
       <div className="flex flex-wrap justify-center mt-20 gap-10">
