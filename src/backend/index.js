7d5509d2e72c2c5313ce104d7bedc9517788fef0
David Asulin <dudyas6@gmail.com>
2023-07-17 15:31:54 +0300

    Update index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const PORT = 3001; // Choose a port number
-const cron = require('node-cron');
+const cron = require("node-cron");
 const vendor_order = require("./models/vendor_order.model.js");
 
 // Server uses
 app.use(express.json());
-app.use(cors({
-  origin: "https://supply-front.vercel.app",
-}));
+app.use(
+  cors({
+    origin: "https://supply-front.vercel.app",
+  })
+);
 
 // Routes
 const itemsRoute = require("./routes/items");
@@ -49,40 +51,36 @@ app.listen(PORT, () => {
 });
 
 // The function do some tasks every 24h
-const AutoTasks = () =>{
+const AutoTasks = () => {
   // define the time -> every day at 06am. (minute-hour-day-month-year)
-  cron.schedule('0 06 * * *', () => {
+  cron.schedule("0 06 * * *", () => {
+    // change status for al orders from vendor_order table
+    ChangeOrderFromVendorStatus();
 
-  // change status for al orders from vendor_order table
-  ChangeOrderFromVendorStatus();
-  
-  // TODO: change status for all orders from users_order table
-  
+    // TODO: change status for all orders from users_order table
   });
-  
-}
-
+};
 
 function ChangeOrderFromVendorStatus() {
   // The function changes status for all orders from vendor_order table.
   vendor_order
-  .find({ Status: { $ne: "Completed" } }) // status not equal to "Completed"
-  .then((orders) => {
-    // update status of each order
-    orders.forEach((order) => {
-      if (order.Status === "Pending") {
-        order.Status = "In Progress";
-      } else if (order.Status === "In Progress") {
-        order.Status = "Completed";
-      }
-    });
+    .find({ Status: { $ne: "Completed" } }) // status not equal to "Completed"
+    .then((orders) => {
+      // update status of each order
+      orders.forEach((order) => {
+        if (order.Status === "Pending") {
+          order.Status = "In Progress";
+        } else if (order.Status === "In Progress") {
+          order.Status = "Completed";
+        }
+      });
 
-    // save the updated orders
-    Promise.all(orders.map((order) => order.save()))
-      .then(() => console.log("Orders updated successfully"))
-      .catch((err) => console.log("Error updating orders: " + err));
-  })
-  .catch((err) => console.log("Error fetching orders: " + err));
+      // save the updated orders
+      Promise.all(orders.map((order) => order.save()))
+        .then(() => console.log("Orders updated successfully"))
+        .catch((err) => console.log("Error updating orders: " + err));
+    })
+    .catch((err) => console.log("Error fetching orders: " + err));
 }
 
 AutoTasks();
